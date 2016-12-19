6535e5dfa6b55743b900a79940b86d82f1bc625f
danielCSS <collelerev@gmail.com>
2016-12-19 15:18:33 +0200

    svg moved menu2 to home

--- framework/js/app.js
+++ framework/js/app.js
@@ -15,30 +15,11 @@ function updateCarousel(l,i)
       break;
     }
   }
-
-
-
-  // console.log(l);
-  // console.log(l[0]);
-  // console.log(l[i]);
-  // console.log("active item " + index);
-  // console.log("previous item " + (index-1) % listLength);
-  // console.log(list[(index-1) % listLength]);
-  // list[(index-1) % listLength].classList.remove('is-prev');
-  // list[index].classList.remove('is-active');
-  // list[index].classList.add('is-prev');
-  // list[(index+1) % listLength].classList.remove('is-next');
-  // list[(index+1) % listLength].classList.add('is-active');
-  // list[(index+2) % listLength].classList.add('is-next');
-
 }
 var list = document.querySelector(".js-aside-banner");
-console.log(list.children);
 var listLength = list.children.length;
-console.log(listLength);
-var activeItem = document.querySelector(".js-aside-banner .is-active");
-var index = Array.prototype.indexOf.call(list.children, activeItem);
-console.log("index = " + index);
 
-//updateCarousel();
+// var activeItem = document.querySelector(".js-aside-banner .is-active");
+// var index = Array.prototype.indexOf.call(list.children, activeItem);
+
 setInterval(updateCarousel, 3000);
