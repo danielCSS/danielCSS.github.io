b6966868e8b3492dcc6bc1cdf2a4a7f156914a9b
danielCSS <collelerev@gmail.com>
2017-01-19 16:27:57 +0200

    added top banner component

--- framework/js/app.js
+++ framework/js/app.js
@@ -6,16 +6,14 @@
   var Carousel = function(element) {
     this.element = element;
     this.dotContainer = document.createElement('div');
-    // short circuit: if no children return 0 (without checking RHS) if children return length
-    //this.numItems = element.children && element.children.length;
     this.numItems = element.children ? element.children.length : 0;
-    //this.slideDuration = 3000 && Number(element.getAttribute("data-duration"));
-    this.slideDuration = element.getAttribute("data-duration") ? Number(element.getAttribute("data-duration")) : 3000;
+    console.log('data-duration: ' + element.getAttribute('data-duration'));
+    this.slideDuration = element.getAttribute('data-duration') ? Number(element.getAttribute('data-duration')) : 3000;
     this.count = 0;
     this.orderIndex = 0;
     this.dotIndex = 0;
+    this.directionValue = element.getAttribute('data-direction') == 'ltr' ? 1 : -1;
   };
-
 
   Carousel.prototype.createDots = function (len) {
     this.dotContainer.classList.add('c-carousel__dots');
@@ -37,7 +35,6 @@
     }
     this.count = 0;
     this.element.style.transform = "translateX(0)";
-    //carousel.orderIndex = !!currentIndex ? (currentIndex - 1) : numItems - 1;
     this.orderIndex = (currentIndex - 1 + this.numItems) % this.numItems;
   };
   Carousel.prototype.updateCarousel = function () {
@@ -46,7 +43,7 @@
     this.dotContainer.children[this.dotIndex].classList.remove('is-active');
     this.dotContainer.children[(this.dotIndex + 1) % this.numItems].classList.add('is-active');
     this.dotIndex = (this.dotIndex + 1) % this.numItems;
-    var str = "translateX(" + (this.count * -100) + "%)";
+    var str = "translateX(" + (this.count * this.directionValue * 100) + "%)";
     this.element.style.transform = str;
     if(this.count + 1 == this.numItems) {
       var that = this;
@@ -66,14 +63,11 @@
     this.orderIndex = this.numItems - 1;
   };
 
-  // Object.defineProperties(Carousel.prototype, {
-  //
-  // });
-
   var carouselElements = document.querySelectorAll('.js-carousel');
   carouselElements = Array.prototype.slice.call(carouselElements);
   var carousels = carouselElements.map(function(carouselElement) { return new Carousel(carouselElement); });
   carousels.forEach(function (carousel) {
+    console.log(carousel);
     carousel.init();
     carousel.dotContainer.addEventListener('click', function(e) {
     	if(e.target && e.target.nodeName == "SPAN") {
@@ -86,73 +80,4 @@
     	}
     });
   });
-
-  /// old:
-
-
-  // var list = document.querySelector(".js-carousel");
-  // var numItems = list.children.length;
-  // var dotContainer = document.createElement('div');
-  // var carousel = {
-  //   orderIndex: 0,
-  //   dotIndex: 0,
-  //   itemDuration: 2000,
-  //   count: 0,
-  //   slide: function() {
-  //     return setInterval(this.updateCarousel, this.itemDuration);
-  //   },
-  //   createDots: function(len) {
-  //     dotContainer.classList.add('c-carousel__dots');
-  //     for(var i = 0;i<len;i++) {
-  //       var dot = document.createElement('span');
-  //       dot.classList.add('c-carousel__dot');
-  //       dotContainer.appendChild(dot);
-  //     }
-  //     list.parentNode.insertBefore(dotContainer, list.nextSibling);
-  //     //initialize first dot to be active
-  //     dotContainer.children[0].classList.add('is-active');
-  //   },
-  //   updateCarousel: function() {
-  //     list.style.transitionProperty = "transform"; //re-enable transform transition
-  //     carousel.count++;
-  //     dotContainer.children[carousel.dotIndex].classList.remove('is-active');
-  //     dotContainer.children[(carousel.dotIndex + 1) % numItems].classList.add('is-active');
-  //     carousel.dotIndex = (carousel.dotIndex + 1) % numItems;
-  //     var str = "translateX(" + (carousel.count * -100) + "%)";
-  //     list.style.transform = str;
-  //     if(carousel.count + 1 == numItems) {
-  //       setTimeout(function() {
-  //         carousel.setOrder(carousel.orderIndex);
-  //       }, 300)
-  //     }
-  //   },
-  //
-  //   setOrder: function(currentIndex) {
-  //     list.style.transitionProperty = "none"; //temorarily disable transition on transform
-  //     for(var i=0; i< numItems; i++) {
-  //       list.children[(currentIndex+i) % numItems].style.order = i;
-  //     }
-  //     carousel.count = 0;
-  //     list.style.transform = "translateX(0)";
-  //     //carousel.orderIndex = !!currentIndex ? (currentIndex - 1) : numItems - 1;
-  //     carousel.orderIndex = (currentIndex - 1 + numItems) % numItems;
-  //   },
-  //
-  //   init: function () {
-  //     this.createDots(numItems);
-  //     this.slide();
-  //     carousel.orderIndex = numItems - 1;
-  //   }
-  // };
-  // dotContainer.addEventListener("click", function(e) {
-  // 	if(e.target && e.target.nodeName == "SPAN") {
-  //       var index = getElIndex(e.target);
-  //       clearInterval(carousel.slide());
-  //       carousel.setOrder(index);
-  //       document.querySelector(".c-carousel__dot.is-active").classList.remove('is-active');
-  //       carousel.dotIndex = index;
-  //       dotContainer.children[index].classList.add('is-active');
-  // 	}
-  // });
-  // carousel.init();
 })();
