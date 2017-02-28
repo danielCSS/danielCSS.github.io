dc314f236cc16369bfc777cfbd26824f01e70066
danielCSS <collelerev@gmail.com>
2017-02-28 14:09:12 +0200

    added first draft of icon component

--- webpacks/src/app.js
+++ webpacks/src/app.js
@@ -3,13 +3,16 @@ import './core-scss/main.scss';
 import React from 'react';
 import { render } from 'react-dom';
 import Button from './components/js/button.js';
-
+import Icon from './components/js/icon.js';
 
 const App = () => {
    return (
   <div>
+    <Icon classes="c-icon" icon="facebook" />
+    <Icon classes="c-icon" icon="google" />
+    <Icon classes="c-icon" icon="twitter" />
     <Button classes="c-btn--primary u-margin-top-small" text="Play Now" />
-    <Button classes="c-btn--primary u-margin-top-small" svg="bla" text="Play Now" />
+    <Button classes="c-btn--primary u-margin-top-small" svg="bla" text="Play bingo Now" />
   </div>
 )};
 //
