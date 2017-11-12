efa4742567e40bbd6b0875c232b20e9212a134dd
danielCSS <collelerev@gmail.com>
2017-11-12 10:17:05 +0200

    webp stuff

--- webp/webp.js
+++ webp/webp.js
@@ -1,7 +1,7 @@
 const imagemin = require("imagemin");
 const imageminWebp = require("imagemin-webp");
 
-imagemin(["img/*.{jpg,png}"], "build/images", {
+imagemin(["img/*.{jpg,png}"], "img", {
   use: [imageminWebp({ quality: 65 })]
 }).then(() => {
   console.log("Images optimized");
