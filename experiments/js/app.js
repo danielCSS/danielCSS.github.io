ea9664525ea393b0a087908b4fb632b8c0e65ab9
daniel <dddavid2@gmail.com>
2019-10-29 16:51:19 +0200

    Update app.js

--- experiments/js/app.js
+++ experiments/js/app.js
@@ -1,7 +1,7 @@
 console.log('hi there!');
 console.log('web workers');
 
-const worker = new Worker('worker.js');
+const worker = new Worker('js/worker.js');
 
 worker.postMessage('hello');
 worker.onmessage = event => {
