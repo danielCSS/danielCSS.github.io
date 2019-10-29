edcf3574f37bb1ec95e9f243dc7481d4d881c554
daniel <dddavid2@gmail.com>
2019-10-29 17:19:34 +0200

    second iteration web workers

--- experiments/js/app.js
+++ experiments/js/app.js
@@ -4,6 +4,16 @@ console.log('web workers');
 const worker = new Worker('js/worker.js');
 
 worker.postMessage('hello');
-worker.onmessage = event => {
+
+// first iteration: Communication with a Web Worker
+
+// worker.onmessage = event => {
+//     console.log(event.data);
+// }
+
+// second iteration: Multiple event listeners
+// If you want to setup multiple listeners for the message event, instead of using onmessage create an event listener (applies to the error event as well):
+
+worker.addEventListener('message', event => {
     console.log(event.data);
-}
+}, false)
--- experiments/js/worker.js
+++ experiments/js/worker.js
@@ -1,9 +1,28 @@
 
-onmessage = event => {
+
+// first iteration: Communication with a Web Worker
+
+// onmessage = event => {
+//     console.log(event.data);
+//     postMessage('hey');
+// }
+
+// onerror = event => {
+//     console.error(event.message);
+// }
+
+
+// second iteration: Multiple event listeners
+
+addEventListener('message', event => {
     console.log(event.data);
     postMessage('hey');
-}
+}, false)
 
-onerror = event => {
-    console.error(event.message);
-}
+addEventListener('message', event => {
+    console.log(`I'm curious and I'm listening too`);
+}, false)
+
+addEventListener('error', event => {
+    console.log(event.message);
+}, false)
