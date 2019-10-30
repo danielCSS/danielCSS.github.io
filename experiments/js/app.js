0b3d259f22e34aa8373265cd480161d4e0f8300d
daniel <dddavid2@gmail.com>
2019-10-30 14:57:37 +0200

    added iframe

--- experiments/js/app.js
+++ experiments/js/app.js
@@ -16,20 +16,32 @@ const worker = new Worker('js/worker.js');
 // second iteration: Multiple event listeners
 // If you want to setup multiple listeners for the message event, instead of using onmessage create an event listener (applies to the error event as well):
 
-// worker.postMessage('hello');
-// worker.addEventListener('message', event => {
-//     // console.log(event.data);
-//     outputDiv.innerHTML += `<p>${event.data}</p>`;
-// }, false)
+worker.postMessage('hello');
+worker.addEventListener('message', event => {
+    // console.log(event.data);
+    outputDiv.innerHTML += `<p>${event.data}</p>`;
+}, false)
 
 
 // third iteration: Using the Channel Messaging API
 
-const data = { name: 'Flavio' };
-const channel = new MessageChannel();
-worker.postMessage(data, [channel.port2]);
+// const data = { name: 'Flavio' };
+// const channel = new MessageChannel();
+// worker.postMessage(data, [channel.port2]);
 
-channel.port1.addEventListener('message', event => {
-  console.log(event.data)
-  outputDiv.innerHTML += `<p>${event.data}</p>`;
-})
+// channel.port1.addEventListener('message', event => {
+//   console.log(event.data)
+//   outputDiv.innerHTML += `<p>${event.data}</p>`;
+// }) (not working)
+
+const channel = new MessageChannel();
+const display = document.querySelector('span');
+const iframe = document.querySelector('iframe');
+
+iframe.addEventListener('load', () => {
+    iframe.contentWindow.postMessage('Hey', '*', [channel.port2]);
+}, false)
+
+channel.port1.onmessage = event => {
+    display.innerHTML = event.data;
+}
--- experiments/js/worker.js
+++ experiments/js/worker.js
@@ -14,31 +14,10 @@
 
 // second iteration: Multiple event listeners
 
-// addEventListener('message', event => {
-//     console.log(event.data);
-//     // postMessage('hey');
-//     postMessage(event.data);
-// }, false)
-
-// addEventListener('message', event => {
-//     console.log(`I'm curious and I'm listening too`);
-//     postMessage(`I'm curious and I'm listening too`);
-// }, false)
-
-// addEventListener('error', event => {
-//     console.log(event.message);
-// }, false)
-
-
-// third iteration: Using the Channel Messaging API
-
 addEventListener('message', event => {
-    console.log('A new message arrived!');
     console.log(event.data);
     // postMessage('hey');
-    const data = { someData: 'hey' };
-
-    event.ports[0].postMessage(data);
+    postMessage(event.data);
 }, false)
 
 addEventListener('message', event => {
@@ -49,3 +28,24 @@ addEventListener('message', event => {
 addEventListener('error', event => {
     console.log(event.message);
 }, false)
+
+
+// third iteration: Using the Channel Messaging API
+
+// addEventListener('message', event => {
+//     console.log('A new message arrived!');
+//     console.log(event.data);
+//     // postMessage('hey');
+//     const data = { someData: 'hey' };
+
+//     event.ports[0].postMessage(data);
+// }, false)
+
+// addEventListener('message', event => {
+//     console.log(`I'm curious and I'm listening too`);
+//     postMessage(`I'm curious and I'm listening too`);
+// }, false)
+
+// addEventListener('error', event => {
+//     console.log(event.message);
+// }, false)
