b0da1e49c86b6507f2d8194bec2119f5f838d767
daniel <dddavid2@gmail.com>
2019-10-30 13:44:41 +0200

    Update app.js

--- experiments/js/app.js
+++ experiments/js/app.js
@@ -6,10 +6,9 @@ outputDiv.innerHTML = '';
 
 const worker = new Worker('js/worker.js');
 
-worker.postMessage('hello');
-
 // first iteration: Communication with a Web Worker
 
+// worker.postMessage('hello');
 // worker.onmessage = event => {
 //     console.log(event.data);
 // }
@@ -17,6 +16,7 @@ worker.postMessage('hello');
 // second iteration: Multiple event listeners
 // If you want to setup multiple listeners for the message event, instead of using onmessage create an event listener (applies to the error event as well):
 
+// worker.postMessage('hello');
 // worker.addEventListener('message', event => {
 //     // console.log(event.data);
 //     outputDiv.innerHTML += `<p>${event.data}</p>`;
