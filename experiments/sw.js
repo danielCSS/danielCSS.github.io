9f8cd2e2cbd9f0941a508cb5822306ddb3424b90
danield770 <dddavid2@gmail.com>
2020-08-24 16:28:18 +0300

    added some css

--- experiments/sw.js
+++ experiments/sw.js
@@ -1,11 +1,32 @@
 const OFFLINE_GIF = "images/notFound.webp";
 
 const CACHE_NAME = "experiments-v1";
+const assets = [
+  "/",
+  "index.html",
+  "css/main.css",
+  "css/pwa.css",
+  "images/notFound.webp",
+  "images/favicon.ico",
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      cache.addAll(["images/favicon.ico", OFFLINE_GIF]);
+      cache.addAll(assets);
+    })
+  );
+});
+
+// activate event
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
     })
   );
 });
