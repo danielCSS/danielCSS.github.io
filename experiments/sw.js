128c0c659b039164c52143f816c0202eca410ae0
danield770 <dddavid2@gmail.com>
2020-08-24 18:43:30 +0300

    Update sw.js

--- experiments/sw.js
+++ experiments/sw.js
@@ -1,35 +1,14 @@
 const OFFLINE_GIF = "images/notFound.webp";
 
 const CACHE_NAME = "experiments-v1";
-const assets = [
-  "/",
-  "index.html",
-  "css/main.css",
-  "css/pwa.css",
-  "images/notFound.webp",
-  "images/favicon.ico",
-];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      cache.addAll(assets);
+      cache.addAll(["images/favicon.ico", OFFLINE_GIF]);
     })
   );
 });
-
-// activate event
-// self.addEventListener("activate", (event) => {
-//   event.waitUntil(
-//     caches.keys().then((keys) => {
-//       return Promise.all(
-//         keys
-//           .filter((key) => key !== CACHE_NAME)
-//           .map((key) => caches.delete(key))
-//       );
-//     })
-//   );
-// });
 
 self.addEventListener("fetch", (event) => {
   console.log(event.request);
