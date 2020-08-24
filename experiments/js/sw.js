38a4558459fde57afefca98cee55c215b79903b1
danield770 <dddavid2@gmail.com>
2020-08-24 11:29:37 +0300

    service worker stuff

---
+++ experiments/js/sw.js
@@ -0,0 +1,15 @@
+const OFFLINE_GIF = "../images/notFound.webp";
+
+const CACHE_NAME = "experiments-v1";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      cache.addAll(["../images/favicon.ico", OFFLINE_GIF]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log(event.request);
+});
