2f8c427312fc999edb0b1aa557ddb08e92193d1a
danield770 <dddavid2@gmail.com>
2020-08-24 10:54:30 +0300

    sw

--- experiments/script.js
+++ experiments/script.js
@@ -1,17 +1,10 @@
-var headers = new Headers();
-// *** I set the header in order to solve the error above:
-// *** The value is set to "/" because this js is included in html file in upper folder.
-// *** I tried even "../" and many more others values...
-headers.append("Service-Worker-Allowed", "/");
-console.log(headers.get("Service-Worker-Allowed"));
-
 window.addEventListener("load", () => {
   if (!navigator.serviceWorker) {
     return;
   }
   navigator.serviceWorker
     .register("sw.js", {
-      scope: "/experiments",
+      scope: "/experiments/",
     })
     .catch((err) => {
       console.log("SW registration failed", err);
